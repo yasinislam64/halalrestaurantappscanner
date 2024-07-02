1946b65cfeff7b05d601de3d23abbb685425a93a
yasinislam64 <yasin.islam1964@gmail.com>
2024-07-02 19:04:55 -0400

    Add files via upload
    This Python script functions as a localized Halal food and beverage app for Brooklyn, New York. It prompts users to select their borough and then offers categories like Burgers, Chinese, Mexican, Desserts and Drinks, locations near the LIU Brooklyn campus, or all options combined. Depending on the choice, the script displays detailed information including addresses and websites of Halal food spots within the selected category, providing a convenient tool for users to explore and find Halal dining options in Brooklyn.

---
+++ Halal Restaurant App Scanner.py
@@ -0,0 +1,497 @@
+import sys
+
+AdoboMexicanGrillwebsite = "https://adobo-mexicangrill.com/"
+AffysPremiumGrillsite = "https://www.affyspremiumgrill.com/"
+BirdiesHotChickensite ="https://bitebirdies.com/brooklyn"
+BirriaManiasite = "https://www.birriamanianyc.com/"
+BKHalalGrillsite = "https://bkhalalgrillny.com/"
+BlazeHalalGrillsite = "https://www.doordash.com/store/blaze-halal-grill-brooklyn-24571525/"
+ChashniSite = "https://chashnibubbletea.com/"
+ChocolateFactory = "https://www.yelp.com/biz/the-ocky-way-chocolate-factory-brooklyn"
+ConeyIslandHouseofWings = "https://www.coneyislandhouseofwings.com/"
+FatimasGrillsite = "https://www.fatimas-grill.com/menu-brooklyn"
+FattaMano = "https://fattamanonyc.com/"
+ForefeathersSite = "https://www.seamless.com/menu/forefeathers-chicken-madness-1624a-east-16th-street-brooklyn/4442872"
+GuacTimeSite = "https://guactimeusa.com/"
+HalalBeeSite = "https://halalbeesburgers.com/"
+HalalMunchiesSite = "https://eathalalmunchies.com/"
+AseerTimeSite = "https://aseertime.com/"
+StackNBurgerSite = "https://stackdburgernyc.com/"
+TexasNBurgerSite = "https://eattexs.com/locations/2028-bath-ave-brooklyn-ny-11214"
+BoraBoraSite = "https://boraborasmoothiecafe.toast.site/menu/bora-bora-smoothie-cafe-bay-ridge"
+LaghmanExpress = "http://laghmanexpress.com/"
+ThreeSevenFiveChickenFriesSite = "https://www.375chicken.com/"
+EightBitNBitesSite = "https://8bitbitesbk.com/"
+BurgerSpot = [
+
+  " All Burgers Spots",
+
+   "       ",
+   
+ "TexasNBurger",
+  "• 203 Neptune Ave ",
+  "Brooklyn, NY 11235",
+ "(718)-513-3484",
+  TexasNBurgerSite,
+   
+  "• 1201 Surf Ave",
+   "Brooklyn, NY 11224",
+   "(347)-462-2705",
+    TexasNBurgerSite,
+
+  "• 2028 Bath Ave",
+  "Brooklyn, NY 11214",
+  "929-366-0916",
+   TexasNBurgerSite,
+
+   "       ",
+
+   "Blaze Halal Grill",
+  "• 2004 Bath Ave, Brooklyn, NY 11214",
+      "(917)933-9858",
+     BlazeHalalGrillsite,
+
+   "       ",
+   
+  "Birdies Hot Chicken",
+  "• 810 Grand St, Brooklyn, NY 11206",
+      "(917) 909-0201",
+     BirdiesHotChickensite,
+
+    "       ",
+   
+    "Fatima's Grill",
+  "• 964 Coney Island Ave, Brooklyn, NY 11230",
+      "(929)234-3198",
+     FatimasGrillsite,
+
+     "       ",
+   
+     "Forefeathers",
+  "• 1624 E 16th St, Brooklyn, NY  11229",
+      "(347) 371-9310",
+      ForefeathersSite,
+
+    "       ",
+         
+      "Halal Bees",
+      "• 378 Myrtle Ave, Brooklyn, NY 112'05",
+       "(718) 559-0111",
+         HalalBeeSite,
+
+       "       ",
+
+         "Halal Munchies",
+    "• 82 Livingston St, Brooklyn, NY 11201",
+        "(347) 987-3495",
+         HalalMunchiesSite,
+
+         "Halal Munchies",
+   "• 7919 5th Avenue, Brooklyn, NY 11209",
+         "(347) 517-4028",
+         HalalMunchiesSite,
+
+       "Halal Munchies",
+    "• 474 Myrtle Ave, Brooklyn, NY 11205",
+       "(718) 636-6112",
+        HalalMunchiesSite,
+       
+        "              ",
+
+
+        "375º Chicken 'n Fries",
+        "• 158 Lawrence St, Brooklyn, NY 11201",
+        ThreeSevenFiveChickenFriesSite,
+
+        "              ",
+
+   
+       "Stack'd Burger ",
+   "• 18 Nevins St, Brooklyn, NY 11217",
+       "(718) 970-0483",
+       StackNBurgerSite,
+
+       " 8-Bit Bites"
+       "• 881 Lexington Ave",
+       EightBitNBitesSite, 
+    
+       "              ",
+   
+       " 8-Bit Bites"
+       "• 967 Bedford Ave",
+       EightBitNBitesSite, 
+
+
+]
+
+HalalChineseSpots = [
+  
+  "Halal Chinese Spots",
+  
+  "              ",
+  
+   " HALAL Xpress Asian Fusion",
+    "• 7524 18th Ave, Brooklyn, NY 11214",
+    "(718) 416-9090",
+  
+  "• 221 Church Ave, Brooklyn, NY 11218",
+      "(718) 975-3151",
+
+      "              ",
+
+     "No Pork Halal Kitchen",
+  "• 50 4th Ave, Brooklyn, NY 11217",
+   
+     "Laghman Express",
+  "• 7524 18th Ave, Brooklyn, NY 11214",
+    "(315) 621-0525",
+    LaghmanExpress,
+  
+]
+
+MexicanSpotsBK = [
+   
+  "Mexican Spots in Brooklyn",
+
+   
+    "            ",
+
+  "Adobo Mexican Grill",
+  "• 1099 Coney Island Ave, Brooklyn, NY 11230", 
+     "(718)450-3714",
+    AdoboMexicanGrillwebsite,
+
+    "             ",
+
+  "Fatima's Grill",
+  "• 964 Coney Island Ave, Brooklyn, NY 11230",
+      "(929)234-3198",
+     FatimasGrillsite,
+  
+   "             ",
+
+  "Birria Mania",
+"• 7508 3rd Ave, Brooklyn, NY 11209",
+    "(718)701-3987",
+   BirriaManiasite,
+
+  "             ",
+
+    "Guac Time",
+"• 391 Jay St, Brooklyn, NY 11201",
+    GuacTimeSite,
+
+     "Guac Time",
+"• 8312 5th Ave, Brooklyn, NY 11209",
+     GuacTimeSite,
+
+     "Guac Time",
+"• 563 Nostrand Ave, Brooklyn, NY 11216",
+     GuacTimeSite,
+
+    "Guac Time",
+"• 1060 Flatbush Ave, Brooklyn, NY 11226",
+     GuacTimeSite,
+
+        "             ",
+
+]
+
+DessertsnDrinksBK = [
+
+   "             ",
+   
+" All Dessert and Drink Places in NY"
+
+     "             ",
+
+"Chashni Bubble Tea",
+"• 1061 Coney Island Ave, Brooklyn, NY 11230",
+    "(929)620-6651",
+     ChashniSite,
+
+    "             ",
+  
+"Chocolate Factory",
+"• 6740 5th Ave, Brooklyn, NY 11220",
+    "(347)560-6080",
+     ChocolateFactory,
+
+     "               ",
+
+         "Aseer Time",
+    "• 509 74th Street New York, New York",
+         AseerTimeSite,
+
+]
+
+
+
+AllHalalFoodSpotsBK =[
+
+  "Halal Restaurants in Brooklyn",   
+
+    "             ",
+  
+  "TexasNBurger",
+    "• 203 Neptune Ave ",
+    "Brooklyn, NY 11235",
+   "(718)-513-3484",
+    TexasNBurgerSite,
+
+    "             ",
+
+    "• 1201 Surf Ave",
+     "Brooklyn, NY 11224",
+     "(347)-462-2705",
+      TexasNBurgerSite,
+
+    "             ",
+
+    "• 2028 Bath Ave",
+    "Brooklyn, NY 11214",
+    "929-366-0916",
+     TexasNBurgerSite,
+
+    "             ",
+
+    "Adobo Mexican Grill",
+  "• 1099 Coney Island Ave, Brooklyn, NY 11230", 
+     "(718)450-3714",
+    AdoboMexicanGrillwebsite,
+
+    "             ",
+
+   "Affy's Premium Grill",
+"• 1909 Coney Island Ave, Brooklyn, NY 11230",
+    "(718)576-6446",
+    AffysPremiumGrillsite,
+
+  "             ",
+  
+   "Birdies Hot Chicken",
+"• 810 Grand St, Brooklyn, NY 11206",
+    "(917) 909-0201",
+   BirdiesHotChickensite,
+
+  "             ",
+
+   "Birria Mania",
+"• 7508 3rd Ave, Brooklyn, NY 11209",
+    "(718)701-3987",
+   BirriaManiasite,
+
+  "             ",
+
+   "BK HALAL GRILL",
+"• 411 Utica Ave, Brooklyn, NY 11213",
+    "(646)677-4070",
+   BKHalalGrillsite,
+
+  "             ",
+
+   "Blaze Halal Grill",
+"• 2004 Bath Ave, Brooklyn, NY 11214",
+    "(917)933-9858",
+   BlazeHalalGrillsite,
+
+    "             ",
+
+     "Chashni Bubble Tea",
+"• 1061 Coney Island Ave, Brooklyn, NY 11230",
+      "(929)620-6651",
+    ChashniSite,
+
+    "             ",
+  
+    "Chocolate Factory",
+   "• 6740 5th Ave, Brooklyn, NY 11220",
+     "(347)560-6080",
+    ChocolateFactory,
+
+   "             ",
+  
+    "Coney Island House of Wings",
+  "• 3100 Ocean Parkway, Brooklyn, NY 11235",
+     "(347)462-3379",
+    ConeyIslandHouseofWings,
+
+    "             ",
+
+     "Fatima's Grill",
+  "• 964 Coney Island Ave, Brooklyn, NY 11230",
+      "(929)234-3198",
+     FatimasGrillsite,
+  
+    "             ",
+  
+     "Fatta Mano",
+  "• 8501 3rd Ave, Brooklyn, NY 11209",
+     FattaMano,
+
+     "             ",
+  
+     "Forefeathers",
+  "• 1624 E 16th St, Brooklyn, NY  11229",
+     ForefeathersSite,
+
+     "             ",
+  
+        "Guac Time",
+     "• 391 Jay St, Brooklyn, NY 11201",
+        GuacTimeSite,
+
+       "Guac Time",
+      "• 8312 5th Ave, Brooklyn, NY 11209",
+        GuacTimeSite,
+
+       "Guac Time",
+      "• 563 Nostrand Ave, Brooklyn, NY 11216",
+         GuacTimeSite,
+
+        "Guac Time",
+      "• 1060 Flatbush Ave, Brooklyn, NY 11226",
+          GuacTimeSite,
+
+        "             ",
+  
+         "Halal Bees",
+      "• 378 Myrtle Ave, Brooklyn, NY 11205",
+         HalalBeeSite,
+
+       "             ",
+  
+         "Halal Munchies",
+    "• 82 Livingston St, Brooklyn, NY 11201",
+        "(347) 987-3495",
+         HalalMunchiesSite,
+
+         "Halal Munchies",
+   "• 7919 5th Avenue, Brooklyn, NY 11209",
+        "(347) 517-4028",
+         HalalMunchiesSite,
+
+       "Halal Munchies",
+    "• 474 Myrtle Ave, Brooklyn, NY 11205",
+       "(718) 636-6112",
+        HalalMunchiesSite,
+
+        "              ",
+
+
+        "375º Chicken 'n Fries",
+        "• 158 Lawrence St, Brooklyn, NY 11201",
+        ThreeSevenFiveChickenFriesSite,
+
+        "              ",
+        
+        " 8-Bit Bites"
+        "• 881 Lexington Ave",
+        EightBitNBitesSite, 
+     
+        "              ",
+    
+        " 8-Bit Bites"
+        "• 967 Bedford Ave",
+        EightBitNBitesSite, 
+
+         "Aseer Time",
+    "• 509 74th Street New York, New York",
+         AseerTimeSite,
+
+        "              ",
+
+         "Middle Eastern Halal Food 69th",
+     "• 6900 4th Ave, Brooklyn, NY 11209",
+
+         "              ",
+
+         "Stack'd Burger ",
+     "• 18 Nevins St, Brooklyn, NY 11217",
+         StackNBurgerSite,
+  ]  
+NearLIU = [  "Guac Time",
+"• 391 Jay St, Brooklyn, NY 11201",
+   GuacTimeSite,
+   
+   "              ",
+
+
+   "375º Chicken 'n Fries",
+   "• 158 Lawrence St, Brooklyn, NY 11201",
+   ThreeSevenFiveChickenFriesSite,
+   
+   "              ",
+
+
+   "Halal Munchies",
+    "• 82 Livingston St, Brooklyn, NY 11201",
+      "(347) 987-3495",
+       HalalMunchiesSite,
+
+    "              ",
+
+    "Halal Bees",
+    "• 378 Myrtle Ave, Brooklyn, NY 11205",
+        HalalBeeSite,
+
+        "              ",
+
+    " 8-Bit Bites"
+    "• 881 Lexington Ave",
+    EightBitNBitesSite, 
+ 
+    "              ",
+
+    " 8-Bit Bites"
+    "• 967 Bedford Ave",
+    EightBitNBitesSite, 
+
+]
+
+
+print ("Assalamu Alaikum!")
+print ("Welcome to the Halal Food and Beverage App Tracker!") 
+location = input("What city are you located in the United States? Type down the following city with its initials or its full name ")
+NewYorkBoroughs = [ 
+  "These are the following boroughs, proceed with its intitials or full name",
+  "Brooklyn", 
+  "Manhattan",
+   "Queens",
+  " Bronx ",
+  "Staten Island"
+]
+if location == "New York" or location == "NY":
+   for loc in NewYorkBoroughs:
+    print(loc)
+else:
+  print("Sorry, that city is not available")
+  sys.exit()
+borough = input("Select the following borough: ")
+BkFilterlist = input("Select your choice:\n"
+   "1. Burgers\n"
+   "2. Chinese\n"
+   "3. Mexican\n"
+   "4. Desserts and Drinks\n"
+   "5. LIU (Brooklyn campus)\n"
+   "6. Everything\n")
+
+if BkFilterlist == "Burgers" or BkFilterlist == "1":
+   for loc in BurgerSpot:
+       print(loc)
+if BkFilterlist == "Chinese" or BkFilterlist == "2":
+   for loc in HalalChineseSpots:
+      print(loc)
+if BkFilterlist == "Mexican" or BkFilterlist == "3":
+    for loc in MexicanSpotsBK:
+      print(loc)
+if BkFilterlist == "Desserts and Drinks" or BkFilterlist == "4":
+    for loc in DessertsnDrinksBK:
+     print(loc)
+if BkFilterlist == "LIU" or BkFilterlist == "5":
+    for loc in NearLIU:
+     print(loc)
+if BkFilterlist == "Everything" or BkFilterlist == "6":
+   for loc in AllHalalFoodSpotsBK:
+       print(loc)
+ 
